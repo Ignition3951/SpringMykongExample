1d5891ae3384d62729614d211930388c9445692c
Utkarsh Srivastava <ignition3951@gmail.com>
2024-06-21 23:01:17 +0530

    Multiple config files import

--- src/main/java/com/mkyong/core/config/AppConfig.java
+++ src/main/java/com/mkyong/core/config/AppConfig.java
@@ -2,11 +2,13 @@ package com.mkyong.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import com.mkyong.core.service.HelloWorld;
 import com.mkyong.core.serviceImpl.HelloWorldImpl;
 
 @Configuration
+@Import({SchedulerConfig.class,CustomerConfig.class})
 public class AppConfig {
 	
 	@Bean(name = "helloWorld")
